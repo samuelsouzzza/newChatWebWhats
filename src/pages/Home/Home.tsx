4f08c408501aece2be04b4ff3d97424be7dcfe43
= <=>
2023-12-20 12:30:32 -0300

    Estrutura das pastas criada

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Container } from './Home.styles.ts';
+
+export const Home = () => {
+  return (
+    <Container>
+      <p>Página principal</p>
+    </Container>
+  );
+};
